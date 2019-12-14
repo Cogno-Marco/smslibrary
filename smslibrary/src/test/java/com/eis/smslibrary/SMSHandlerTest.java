9c84ed736fd0fa5d046848d844572fea792c8c2d
Cogno_Marco <marco.cognolato.98@gmail.com>
2019-12-14 12:21:10 +0100

    Optimized imports Formatted code

--- smslibrary/src/test/java/com/eis/smslibrary/SMSHandlerTest.java
+++ smslibrary/src/test/java/com/eis/smslibrary/SMSHandlerTest.java
@@ -1,59 +1,55 @@
 package com.eis.smslibrary;
 
 import android.content.Context;
-import android.content.IntentFilter;
 import android.telephony.SmsManager;
 
-import com.eis.smslibrary.listeners.SMSSentListener;
-
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SMSHandlerTest {
 
-    @Mock SmsManager managerMock;
-    @Mock Context contextMock;
+    @Mock
+    SmsManager managerMock;
+    @Mock
+    Context contextMock;
 
     private final String PEER_TEXT = "+393423541601";
     private final String MESSAGE_TEXT = "valid text";
-    private final String SENT_TEXT = (char)0x02 + MESSAGE_TEXT;
+    private final String SENT_TEXT = (char) 0x02 + MESSAGE_TEXT;
     private final SMSPeer VALID_PEER = new SMSPeer(PEER_TEXT);
     private final SMSMessage VALID_MESSAGE = new SMSMessage(VALID_PEER, MESSAGE_TEXT);
     private SMSHandler instance;
 
     @Before
-    public void setup(){
+    public void setup() {
         instance = SMSHandler.getInstance();
         instance.clearSetup();
     }
 
     @Test(expected = IllegalStateException.class)
-    public void sendMessageWithNullContext_throws(){
+    public void sendMessageWithNullContext_throws() {
         instance.sendMessage(VALID_MESSAGE);
     }
 
     @Test
-    public void singletonInstance(){
+    public void singletonInstance() {
         assertEquals(SMSHandler.getInstance(), SMSHandler.getInstance());
     }
 
     @Test(expected = NullPointerException.class)
-    public void setupNullContext_throws(){
+    public void setupNullContext_throws() {
         instance.setup(null);
     }
 
     @Test()
-    public void validMessage_isSent(){
+    public void validMessage_isSent() {
         SMSCore.setManager(managerMock);
         instance.setup(contextMock);
         instance.sendMessage(VALID_MESSAGE);
@@ -61,7 +57,7 @@ public class SMSHandlerTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void nullMessage_throws(){
+    public void nullMessage_throws() {
         instance.setup(contextMock);
         instance.sendMessage(null);
     }
