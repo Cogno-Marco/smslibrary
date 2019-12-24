8507fcd7745f1dedfd6591b86667f6ef8435da73
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-12-24 22:31:23 +0100

    fixed imports

--- smslibrary/src/test/java/com/eis/smslibrary/SMSPartTest.java
+++ smslibrary/src/test/java/com/eis/smslibrary/SMSPartTest.java
@@ -3,7 +3,9 @@ package com.eis.smslibrary;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Giovanni Velludo
