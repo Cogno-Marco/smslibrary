8a365c5892441ebf2c22f057974237d1eb138401
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2020-01-07 02:22:36 +0100

    Updated the "HIDDEN_CHARACTER"

--- smslibrary/src/main/java/com/eis/smslibrary/SMSMessageHandler.java
+++ smslibrary/src/main/java/com/eis/smslibrary/SMSMessageHandler.java
@@ -46,7 +46,7 @@ public class SMSMessageHandler implements MessageHandler<String, String, SMSMess
     /**
      * Interprets a string arrived via the communication channel and parses it to a library {@link SMSMessage}.
      *
-     * @param peerData Data about the peer coming from the sms pdus.
+     * @param peerData    Data about the peer coming from the sms pdus.
      * @param messageData Data about the message coming from the sms pdus.
      * @return The message if the string has been parsed correctly, null otherwise.
      * @author Matteo Carnelos
@@ -55,8 +55,9 @@ public class SMSMessageHandler implements MessageHandler<String, String, SMSMess
         try {
             SMSPeer peer = new SMSPeer(peerData);
             return parseStrategy.parseMessage(messageData, peer);
+        } catch (InvalidTelephoneNumberException | IllegalArgumentException e) {
+            return null;
         }
-        catch(InvalidTelephoneNumberException | IllegalArgumentException e) { return null; }
     }
 
     /**
@@ -71,7 +72,7 @@ public class SMSMessageHandler implements MessageHandler<String, String, SMSMess
 
     public class DefaultSMSMessageParseStrategy implements MessageParseStrategy<String, SMSPeer, SMSMessage> {
 
-        static final String HIDDEN_CHARACTER = "\r";
+        static final String SIGNATURE = "_by smslib_\n";
 
         /**
          * Parses sms data into a SMSMessage if possible
@@ -83,7 +84,7 @@ public class SMSMessageHandler implements MessageHandler<String, String, SMSMess
         @Override
         public SMSMessage parseMessage(@NonNull final String channelData, @NonNull final SMSPeer channelPeer) {
             //First character of the content must be the hidden char
-            if (!channelData.startsWith(HIDDEN_CHARACTER))
+            if (!channelData.startsWith(SIGNATURE))
                 return null;
             String messageData = channelData.substring(1);
             return new SMSMessage(channelPeer, messageData);
@@ -97,7 +98,7 @@ public class SMSMessageHandler implements MessageHandler<String, String, SMSMess
          */
         @Override
         public String parseData(@NonNull final SMSMessage message) {
-            return HIDDEN_CHARACTER + message.getData();
+            return SIGNATURE + message.getData();
         }
     }
 
